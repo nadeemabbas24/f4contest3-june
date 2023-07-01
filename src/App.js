61b2b0fe400865df30ab5e0b94c6e20e572bc5e9
nadeem abbas <nadeemmeerza@gmail.com>
2023-07-01 22:25:34 +0530

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Navbar from './components/Navbar';
-import { Provider } from 'react-redux';
-import store from './store';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route exact path='/' Component={Navbar}>
-             <Route path='/home' Component={Home} />
-             <Route path='/cart' Component={Cart} />
-          </Route>          
+          <Route exact path="/" Component={Navbar}>
+            <Route path="/" Component={Home} />
+            <Route path="/cart" Component={Cart} />
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-         <nav>
-      <div className="brand">
-        <Link to="/home">Shopping Cart</Link>
-      </div>
-      <div className="nav-items">
-        <Link to="/home">Home</Link>
-        <Link to="/cart" className="mycart">Cart</Link>
-
-      </div>
-    </nav>
-    <Outlet/>
+      <nav>
+        <div className="brand">
+          <Link to="/">Shopping Cart</Link>
+        </div>
+        <div className="nav-items">
+          <Link to="/">Home</Link>
+          <Link to="/cart" className="mycart">
+            Cart
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
