132cb36c656f0ebbfca551762489ef2fcb66e5c9
nadeem abbas <nadeemmeerza@gmail.com>
2023-07-01 22:30:22 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
